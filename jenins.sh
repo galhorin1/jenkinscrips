a5e4e5a4f018c9e85ed930c201b1aa72c387d60f
Gal Horin <75362961+galhorin1@users.noreply.github.com>
2022-07-20 13:23:25 +0300

    Update jenins.sh

--- jenins.sh
+++ jenins.sh
@@ -4,7 +4,7 @@ sudo apt install net-tools
 sudo apt-get install openssh-server openssh-client
 sudo apt install openjdk-8-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo aptkey add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list’
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 audo apt-get install jenkins
 sudo apt install python
